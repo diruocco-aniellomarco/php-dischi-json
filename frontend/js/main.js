3a3c35989ce20955e8c6f35642a83bded8dd0c58
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-09-28 19:49:33 +0200

    API in PHP

---
+++ frontend/js/main.js
@@ -0,0 +1,26 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      dischiList: [],
+    };
+  },
+
+  mounted() {
+    // console.log(axios);
+    axios
+      .get("http://localhost/php-dischi-json/backend/dischi.php")
+      .then((response) => {
+        this.dischiList = response.data;
+        console.log(this.dischiList[0]);
+      });
+  },
+}).mount("#app");
+
+// axios
+//   .get("http://localhost/php-dischi-json/backend/dischi.php")
+//   .then((response) => {
+//     this.dischi = response.data;
+//     console.log(this.dischi);
+//   });
---
+++ readme.md
@@ -0,0 +1,15 @@
+**Esercizio di oggi: PHP Dischi JSON**
+
+**Descrizione**
+
+Dobbiamo creare una web-app che permetta di leggere una lista di dischi presente nel nostro server.
+
+_Stack_
+
+Html, CSS, VueJS (importato tramite CDN), axios, PHP
+
+_Consigli_
+
+Nello svolgere l'esercizio seguite un approccio graduale.
+Prima assicuratevi che il vostro frontend (app Vue) riesca a comunicare correttamente con il vostro backend (API in PHP).
+Solo a questo punto sarà utile passare alla lettura della lista da un file JSON.
